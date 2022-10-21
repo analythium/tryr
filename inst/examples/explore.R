2fffc1a5cb886999ae3801e9207aa269fe19f863
Peter Solymos <psolymos@gmail.com>
2022-10-21 15:48:53 -0600

    Add handlers to apps
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- inst/examples/explore.R
+++ inst/examples/explore.R
@@ -18,6 +18,29 @@ plumber_fun <-function() {
             plumber::forward()
         }) |>
         plumber::pr_set_debug(DEBUG) |>
+        plumber::pr_set_404(
+            function(req, res) {
+                tryr::msg(
+                title = paste0(
+                    "Status 404: ", 
+                    tryr::http_status_codes["404", "message"]),
+                level = "INFO"
+                )
+                tryr::http_handler(req, res, 404L)
+            }
+        ) |>
+        plumber::pr_set_error(
+            function(req, res, err) {
+                tryr::msg(
+                title = paste0(
+                    "Status 500: ", 
+                    tryr::http_status_codes["500", "message"]),
+                message = err,
+                level = "ERROR"
+                )
+                tryr::http_handler(req, res, 500L)
+            }
+        ) |>
         plumber::pr_hooks(
             list(
                 preroute = function(data, req, res) {
