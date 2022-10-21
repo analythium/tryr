528544a4ba864d6ad9d9eb8e491de363db63b37a
Peter Solymos <psolymos@gmail.com>
2022-10-21 15:48:29 -0600

    Add http_handler
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- R/http_error.R
+++ R/http_error.R
@@ -3,6 +3,9 @@
 #' @param status HTTP status code.
 #' @param message Error message.
 #' @param body A list, additional values to be returned.
+#' @param req The request object.
+#' @param res The response object.
+#' @param ... Other arguments passed to `http_response`.
 #' 
 #' @return `http_error` returns an error with a custom condition attribute.
 #'  `http_success` returns a list.
@@ -56,7 +59,25 @@ http_success <- function(
     succ <- http_status_codes[http_status_codes$status < 400,]
     if (!(status %in% succ$status))
         stop("Unrecognized status code.")
-    i <- as.list(succ[as.character(status),])
+    i <- http_response(
+        status = status,
+        message = message,
+        body = body)
+    class(i) <- c("http_success", "http_response")
+    i
+}
+
+#' @rdname http-messages
+#' @export 
+http_response <- function(
+    status = 200L,
+    message = NULL,
+    body = NULL
+) {
+    status <- as.integer(status)
+    if (!(status %in% http_status_codes$status))
+        stop("Unrecognized status code.")
+    i <- as.list(http_status_codes[as.character(status),])
     i[["message"]] <- paste0(
         # "Status ", status, ": ", 
         i[["message"]], 
@@ -66,5 +87,14 @@ http_success <- function(
         i$body <- body
     structure(
         i,
-        class = "http_success")
+        class = c("http_response", "list"))
+}
+
+#' @rdname http-messages
+#' @export 
+http_handler <- function(req, res, status, ...) {
+    x <- http_response(status = status, ...)
+    res$status <- x$status
+    res$body <- x
+    x
 }
